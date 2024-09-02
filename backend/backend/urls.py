c0f4dcd48a4f575f801296dca882d22c494bcadd
Viktor <lopatqin@gmail.com>
2024-09-02 18:31:17 +0700

    flake8 4

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,8 +1,8 @@
 # Импорты сторонних# Импорты сторонних библиотек
+from api import views
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from api import views
 
 router = routers.DefaultRouter()
 router.register('tasks', views.TaskView, 'task')
